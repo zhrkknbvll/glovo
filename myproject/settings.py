54008dfb947d931e496dac394d5f829210c9c757
ZHRKKNBVLL <aidanawzx@icloud.com>
2026-02-01 08:31:10 +0600

    req.txt

--- myproject/settings.py
+++ myproject/settings.py
@@ -110,6 +110,9 @@ MODELTRANSLATION_LANGUAGES = ('en', 'ru')
 # Static and Media
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
